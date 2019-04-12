19a00d242b210fe2848cd2f702aca59fb8fb1b06
Lijun Ma <lijum@amazon.com>
2019-04-12 15:27:10 -0700

    change to gamma end point to test TestLogDriverSecretSupport

--- agent/functional_tests/tests/functionaltests_unix_test.go
+++ agent/functional_tests/tests/functionaltests_unix_test.go
@@ -634,6 +634,9 @@ func fluentdDriverTest(taskDefinition string, t *testing.T) {
 // TestLogDriverSecretSupport tests the log driver secret support using
 // the awslogs and awslogs-multiline-pattern option is utilized as a secret storing as an unencrypted parameter
 func TestLogDriverSecretSupport(t *testing.T) {
+	var ECS *ecs.ECS
+	ECS.Client.Endpoint = "https://madison.us-west-2.amazonaws.com"
+
 	RequireDockerVersion(t, ">=17.06.2")
 
 	if os.Getenv("TEST_DISABLE_EXECUTION_ROLE") == "true" {
